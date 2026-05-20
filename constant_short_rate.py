48511cf2dfd4c3c448e44b822e4fd76b9bd68732
Henrique <henrique.dias@ievent.pt>
2026-05-20 23:55:29 +0400

    Initial commit

---
+++ constant_short_rate.py
@@ -0,0 +1,14 @@
+from get_year_deltas import *
+
+class constant_short_rate(object):
+    def __init__(self, name, short_rate):
+        self.name = name
+        self.short_rate = short_rate
+
+    def get_discount_factors(self, date_list, dtobjects = True):
+        if dtobjects is True:
+            dlist = get_year_deltas(date_list)
+        else:
+            dlist = np.array(date_list)
+        dflist = np.exp(self.short_rate * np.sort(-dlist))
+        return np.array((date_list, dflist)).T
---
+++ main.py
@@ -0,0 +1,5 @@
+from sn_random_numbers import *
+
+snrn = sn_random_numbers((2, 2, 2), anthithetic = False, moment_matching = False, fixed_seed = True)
+
+print(snrn)
